a70b95d45602aac2c465747abe77de1252635654
Hadasa <hadasagoldberg@gmail.com>
2022-11-17 13:21:09 -0300

    fix not found

--- Usuarios/views.py
+++ Usuarios/views.py
@@ -81,8 +81,12 @@ def traverse(request, **kwargs):
 # oid - time
 @api_view(["GET"])
 def oread(request, **kwargs):
+    print("1")
     if request.method == "GET":
+        print("2")
         params = separar_link(kwargs["link"])
+        print("3")
+        print("params", params)
         if len(params) == 2:
             values = redis_instance.execute_command('INGRID.OREAD', params[0], params[1])
             return Response(values, status=200)
@@ -136,3 +140,4 @@ def getconfigfo(request, **kwargs):
             return Response(values, status=200)
         else:
             return Response("Faltan Parametros.", status=404)
+
